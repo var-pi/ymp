da5f837efda449af1b5fe73c5c2c7c1fd130e73e
Stefan Ehin <var-pi@vortex.local>
2025-12-13 11:52:32 +0200

    Restructure the flake and expose only ym.py

--- flake.nix
+++ flake.nix
@@ -1,29 +1,44 @@
 {
-  description = "Music CLI";
+    description = "Music CLI.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { nixpkgs, ... }: let
-    system = "aarch64-darwin"; #Only for Apple Silicon
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      pname = "music-cli";
-      version = "0.1";
-
-      src = ./.;
-      nativeBuildInputs = [ pkgs.makeWrapper pkgs.zsh ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp bin/* $out/bin/
-      '';
-
-      meta = with pkgs.lib; {
-        description = "CLI for music downloading and playback";
-        license = licenses.mit;
-        #platforms = pkgs.platforms.unix;
-      };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
     };
-  };
+
+    outputs = { self, nixpkgs }:
+        let
+            system = "aarch64-darwin"; # change if needed
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            packages.${system}.default =
+                pkgs.stdenv.mkDerivation {
+                    pname = "ym";
+                    version = "0.1";
+
+                    src = ./.;
+
+                    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+                    buildInputs = [
+                        pkgs.python3
+                        pkgs.fzf
+                        pkgs.ffmpeg
+                        pkgs.yt-dlp
+                    ];
+
+                    installPhase = ''
+                    mkdir -p $out/bin
+                    cp $src/ym.py $out/bin/ym
+                    chmod +x $out/bin/ym
+
+                     wrapProgram $out/bin/ym \
+                          --prefix PATH : ${pkgs.lib.makeBinPath [
+                            pkgs.python3
+                            pkgs.fzf
+                            pkgs.ffmpeg
+                            pkgs.yt-dlp
+                        ]}
+                    '';
+                };
+        };
 }
