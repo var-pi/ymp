e6de55deb494b0c5b82019005780216a1daf8ecf
Stefan Ehin <stefanehin4@gmail.com>
2025-12-14 17:06:37 +0200

    Fix playlist playback

--- ymppy/commands/playlist/play.py
+++ ymppy/commands/playlist/play.py
@@ -5,6 +5,9 @@ from ymppy.paths import playlist_dir, library_dir
 def play(loop: bool = False):
     """Play a playlist."""
     songs = cat(playlist_dir / pick(ls(playlist_dir)))
-    while loop:
+    print(songs)
+    while True:
         for song in songs:
             _play(library_dir / song)
+        if not loop:
+            break
