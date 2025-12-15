e72fe781b91b854244aa188832e42c9d03a2b1cc
Stefan Ehin <stefanehin4@gmail.com>
2025-12-15 14:47:46 +0200

    Do not show metadata in delete fzf

--- ymppy/commands/song/delete.py
+++ ymppy/commands/song/delete.py
@@ -3,4 +3,4 @@ from ymppy.paths import library_dir
 
 def delete():
     """Remove a song from library."""
-    rm(library_dir / pick(ls(library_dir)))
+    rm(library_dir / pick(ls(library_dir), with_nth="2..-2"))
