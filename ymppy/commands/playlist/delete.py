475235fd97eeab0ecd7a3e23caf132429d9498b4
Stefan Ehin <stefanehin4@gmail.com>
2025-12-16 10:18:16 +0200

    Do not use basename on playlists

--- ymppy/commands/playlist/delete.py
+++ ymppy/commands/playlist/delete.py
@@ -1,11 +1,11 @@
 import os
 
 import typer
-from ymppy.utils import pick, ls, basename
+from ymppy.utils import pick, ls
 from ymppy.paths import playlist_dir
 
 def delete():
     """Delete a playlist."""
     playlist_title = pick(ls(playlist_dir))
     os.remove(playlist_dir / playlist_title)
-    typer.echo(f"Playlist {basename(playlist_title)} has been deleted")
+    typer.echo(f"Playlist {playlist_title} has been deleted")
