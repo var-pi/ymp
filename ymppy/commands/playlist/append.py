ccdfa6b29290d77ee596a267ce0b72cbf089e528
Stefan Ehin <stefanehin4@gmail.com>
2025-12-16 11:05:52 +0200

    A song can belong up to a single playlist

--- ymppy/commands/playlist/append.py
+++ ymppy/commands/playlist/append.py
@@ -7,11 +7,12 @@ def append():
     Prompt the user to pick a playlist and a song from the library,
     then append the chosen song name to the selected playlist file.
     """
+    playlisted_songs = [s for pl in ls(playlist_dir) for s in cat(playlist_dir / pl)]
+
+    #unplaylisted_songs = [s for s in ls(library_dir) if s not in playlisted_songs]
+    unplaylisted_songs = list(set(ls(library_dir))-set(playlisted_songs))
+    song_title = pick(unplaylisted_songs, with_nth="2..2")
     playlist_title = pick(ls(playlist_dir))
-    playlist_path = playlist_dir / playlist_title
-    typer.echo(cat(playlist_path))
-    songs_not_in_playlist = [s for s in ls(library_dir) if s not in cat(playlist_path)]
-    song_title = pick(songs_not_in_playlist, with_nth="2..2")
-    _append(playlist_path, song_title)
+    _append(playlist_dir / playlist_title, song_title)
 
     typer.echo(f"Added '{basename(song_title)}' to playlist '{playlist_title}'.")
